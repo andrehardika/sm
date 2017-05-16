3a2890a98352e78bc5e0b30b2a04bad0c1d6a72e
Andre <andrehardika@gmail.com>
2017-05-16 14:24:21 +0700

    Sunting selesai dengan beberapa kekurangan

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -101,4 +101,78 @@ class Produk extends MY_Controller
 
 		$this->go('produk');
 	}
+
+	public function sunting($code = NULL)
+	{
+		if (is_null($code) || empty($code)) {
+			$this->message('Produk tidak ditemukan', 'warning');
+			$this->go('produk');
+		}else{
+			$data['product'] = $this->products_m
+			->with_category('fields:name')
+			->with_producer('fields:name')
+			->get(array('code' => $code));
+
+			$data['categories'] = $this->categories_m
+			->fields('id,name')
+			->get_all();
+
+			$data['producers'] = $this->producers_m
+			->fields('id,name')
+			->get_all();
+
+			$this->generateCsrf();
+			$this->render('admin/products/edit', $data);
+		}
+	}
+
+	public function ubah($id= NULL)
+	{
+		if (is_null($id) || empty($id)) {
+			$this->message('Produk tidak ditemukan', 'warning');
+		}else{
+			$input = $this->input->post();
+
+			//jika id kategori ada isinya
+			if (!is_null($input['category_id']) || !empty($input['category_id'])) {
+			//ambil hanya id kategori
+				$input['category_id'] = $this->categories_m->splitIdAndName($input['category_id']);
+			//cek keberadaan kategori
+				if (!$this->categories_m->checkDataAvailabilityById($input['category_id'])) {
+					$this->message('Terjadi kesalahan pada sistem. Ikuti format pengisian kategori produk dengan benar');
+					$this->go('produk/sunting');
+				}
+			}
+
+			//jika id produsen ada isinya
+			if (!is_null($input['producer_id']) || !empty($input['producer_id']))  {
+			//ambil hanya id produsen
+				$input['producer_id'] = $this->producers_m->splitIdAndName($input['producer_id']);
+			//cek keberadaan produsen
+				if (!$this->producers_m->checkDataAvailabilityById($input['producer_id'])) {
+					$this->message('Terjadi kesalahan pada sistem. Ikuti format pengisian produsen produk dengan benar');
+					$this->go('produk/sunting');
+				}
+			}
+
+			if (!isset($input['tax'])) {
+				$input['tax'] = NULL;
+			}
+
+			//update ke db
+			$query = $this->products_m
+			->from_form(NULL, array(
+				'category_id' => $input['category_id'],
+				'producer_id' => $input['producer_id'],
+				'tax' => $input['tax']
+				), array('id' => $id))
+			->update();
+			if ($query === FALSE) {
+				$this->message('Produk gagal disunting.', 'warning');
+			}else{
+				$this->message('Produk berhasil disunting.', 'success');
+			}
+		}
+		$this->go('produk');
+	}
 }
